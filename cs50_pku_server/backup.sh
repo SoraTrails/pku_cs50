335a89242ad671b204087a13520460954abe95f8
SoraTrails <me876657621@126.com>
2019-03-19 09:50:07 +0800

    v1.4 ready to deploy2

--- cs50_pku_server/backup.sh
+++ cs50_pku_server/backup.sh
@@ -5,6 +5,13 @@ else
     submited_path=${SUBMITED_PATH}
 fi
 
+clean(){
+    rm -rf ${submited_path}/speller/dictionary.*
+    rm -f ${submited_path}/speller/check.tmp
+    rm -f ${submited_path}/speller/speller
+    rm -rf ${submited_path}/speller/*.o
+}
+
 text_path=tscs50
 keys_path=kscs50
 path=${1}
@@ -15,20 +22,20 @@ if [ -z ${stuid} -o ! -f ${path}/${name} ]; then
     exit 4
 fi
 
-rm -rf ${submited_path}/speller/dictionary.*
-rm -f ${submited_path}/speller/check.tmp
+# rm -rf ${submited_path}/speller/dictionary.*
+# rm -f ${submited_path}/speller/check.tmp
 
 # unzip /root/speller.zip 
 unzip -P `echo ${stuid} | base64 -i` ${path}/${name} -d ${submited_path}/speller 2>&1 > /dev/null
 cd ${submited_path}/speller/
 
 rm -f core speller *.o
-clang -fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wshadow   -c -o speller.o speller.c
-clang -fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wshadow   -c -o dictionary.o dictionary.c
-clang -fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wshadow -o speller speller.o dictionary.o 
+clang -fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wshadow   -c -o speller.o speller.c 2>&1 > /dev/null
+clang -fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wshadow   -c -o dictionary.o dictionary.c 2>&1 > /dev/null
+clang -fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -Qunused-arguments -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wshadow -o speller speller.o dictionary.o 2>&1 > /dev/null
 
 if [ $? != 0 -o ! -f ./speller ]; then
-    make clean
+    clean
     exit 1
 fi
 
@@ -40,6 +47,7 @@ for file in $filelist ;do
     # res=$(/usr/bin/time -f "%e %x" ./speller ./dictionaries/large ./${text_path}/${file} 2>&1 > ./tmp/${file}.tmp)
     ./speller ./dictionaries/large ./${text_path}/${file} > ./tmp/${file}.tmp
     if [ $? != 0 ];then
+        clean
         exit 2
     fi
     cat ./tmp/${file}.tmp | head --lines=-6 > ./tmp/${file}
@@ -47,6 +55,7 @@ for file in $filelist ;do
 	temp2=(`md5sum ./${keys_path}/${file}`)
 	if [ ${temp1[0]} != ${temp2[0]} ];then
 	    echo "error: ${file}"
+        clean
         exit 3
 	fi
     res=$(tail -2 ./tmp/${file}.tmp | awk '{print $4}')
@@ -54,10 +63,6 @@ for file in $filelist ;do
     rm -f ./tmp/${file}*
 done
 
-rm -rf ${submited_path}/speller/dictionary.*
-rm -f ${submited_path}/speller/check.tmp
-rm -f ${submited_path}/speller/speller
-rm -rf ${submited_path}/speller/*.o
-
 echo -n ${sum}
+clean
 exit 0
